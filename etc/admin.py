1a5eee7e01c616d0da730d4c23d090e38f18dfa7
idle sign <idlesign@yandex.ru>
2016-12-18 18:18:58 +0700

    CHANGELOG updated

---
+++ etc/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# from .models import MyModel
+
+
+# class MyModelAdmin(admin.ModelAdmin):
+#     pass
+#
+# admin.site.register(MyModel, MyModelAdmin)
---
+++ etc/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+# from django.views.generic.base import View
+# from django.shortcuts import redirect
+# from django.shortcuts import get_object_or_404
+# from django.contrib.auth.decorators import login_required
+# from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+
+# @login_required
+# def index(request):
+#     return render(request, 'etc/index.html', {})
