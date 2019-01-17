a7a759bb58ff2bda6be54377d9640bc0ca9ba5d8
idle sign <idlesign@yandex.ru>
2019-01-17 19:14:56 +0700

    No functional changes. Celebrating 1.0.0.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
 
     classifiers=[
         # As in https://pypi.python.org/pypi?:action=list_classifiers
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
